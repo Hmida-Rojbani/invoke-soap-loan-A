93cf4b28d8879bfe99e7ba15de3df3fda6ac2dee
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-04-20 14:27:40 +0100

    results

--- src/main/java/de/tekup/loan/invoke/ui/controllers/CheckController.java
+++ src/main/java/de/tekup/loan/invoke/ui/controllers/CheckController.java
@@ -1,13 +1,20 @@
 package de.tekup.loan.invoke.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import de.tekup.loan.invoke.client.SoapClient;
 import de.tekup.loan.soap.api.consume.loaneligebilty.CustomerRequest;
+import de.tekup.loan.soap.api.consume.loaneligebilty.WsResponse;
 
 @Controller
 public class CheckController {
+	@Autowired
+	private SoapClient client;
 	
 	@GetMapping("/check/customer")
 	public String checkCustomerForm(Model model) {
@@ -16,5 +23,13 @@ public class CheckController {
 		model.addAttribute("request", customerRequest);
 		return "request";
 	}
-
+	
+	@PostMapping("/check/customer")
+	public String checkResults(@ModelAttribute("request") CustomerRequest request,
+			Model model) {
+		//invoke of web service
+		WsResponse response = client.getLoanStatus(request);
+		model.addAttribute("response",response);
+		return "results";
+	}
 }
