4c8f783b29704dd870cb99ab218048559383382e
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-04-20 12:57:27 +0100

    generate

---
+++ src/main/java/de/tekup/loan/invoke/InvokeSoapLoanAApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.loan.invoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvokeSoapLoanAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvokeSoapLoanAApplication.class, args);
+	}
+
+}
